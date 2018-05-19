e4c984ff333bc8577182a472258b1172ef4a0957
Rishi Kumar <chawda.rishii@gmail.com>
2018-05-19 13:18:35 +0530

    add random english word pair

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final word_pair = new WordPair.random();
+
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text('Welcome to Flutter'),
+          title: new Center(
+            child: new Text('Welcome to Flutter'),
+          ) 
         ),
         body: new Center(
-          child: new Text('Hello World'),
+          // child: new Text('Hello World'),
+          child: new Text(word_pair.asPascalCase),
         ),
       ),
     );
