8d4769b923c5ebf8e2cc48fb527604d040e4ae79
Rishi Kumar <chawda.rishii@gmail.com>
2018-05-19 13:42:11 +0530

    till s4.2

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,6 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    final word_pair = new WordPair.random();
-
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
@@ -19,9 +16,58 @@ class MyApp extends StatelessWidget {
         ),
         body: new Center(
           // child: new Text('Hello World'),
-          child: new Text(word_pair.asPascalCase),
+          child: new RandomWords(),
         ),
       ),
     );
   }
+}
+
+// class RandomWords extends StatefulWidget {
+//   @override
+//   State<StatefulWidget> createState() {
+//       // TODO: implement createState
+      
+//     }
+// }
+
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+
+  final _suggestions = <WordPair>[];
+  final _bigger_font = const TextStyle(fontSize: 16.0);
+
+  @override
+  Widget build(BuildContext context) {
+    final word_pair = new WordPair.random();
+    return new Text(word_pair.asPascalCase);
+  }
+
+  Widget _build_suggestions(){
+    return new ListView.builder(
+
+      padding: const EdgeInsets.all(16.0),
+
+      itemBuilder: ( context , i ) {
+
+        if (i.isOdd) {
+          return new Divider();
+        }
+
+        final index = i ~/ 2; // Divides i by 2 and returns an integer. (Ex :- 5 ~/ 2 = 2)
+
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(11));
+
+        }
+
+        return _buildRow(_suggestions[index]);
+      }
+    );
+  }
+
 }
